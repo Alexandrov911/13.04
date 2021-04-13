58893dbb055bf3a281e623db6c640251944faf57
Alexandrov911 <sashok17062002@mail.ru>
2021-04-13 11:35:23 +0300

    13.04

---
+++ Александров1/Александров1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Александров1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число c");
+            double c = Convert.ToDouble(Console.ReadLine());
+            if ( (a < b + c) & (b < a + c) & (c < a + b))
+            {
+                Console.WriteLine("существует");
+            }
+            else
+            {
+                Console.WriteLine("не существует");
+
+            }
+            Console.ReadKey();
+
+       
+        }
+    }
+}
